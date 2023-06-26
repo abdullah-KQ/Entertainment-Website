0a43fec4253bd053205d6aa36cc2c9a42ff2d406
abdullah-KQ <120243031+abdullah-KQ@users.noreply.github.com>
2023-06-26 16:22:50 +0300

    0.4

--- EntertainmentWebsite/settings.py
+++ EntertainmentWebsite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MainApp',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -119,8 +120,27 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static/",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_IMAGE_BACKEND = "pillow"
+CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js' 
+
+CKEDITOR_CONFIGS = {
+    'default':
+        {
+            'toolbar': 'full',
+            'width': 'auto',
+            'extraPlugins': ','.join([
+                'codesnippet',
+            ]),
+        },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class Article(models.Model):
+    pass
